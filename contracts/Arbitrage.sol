a989a26ad7f8af4ae9b7bf2282bb9bd576989d7e
FUJITA KAZUHISA <>
2021-11-30 17:52:21 +0900

    V3toV2 smart contract

--- contracts/Arbitrage.sol
+++ contracts/Arbitrage.sol
@@ -1,6 +1,8 @@
 pragma solidity > 0.6.6;
 
 import './libraries/UniswapV2Library.sol';
+import './libraries/TransferHelper.sol';
+import './libraries/LowGasSafeMath.sol';
 import './interfaces/V2/IUniswapV2Router02.sol';
 import './interfaces/V2/IUniswapV2Pair.sol';
 import './interfaces/V2/IUniswapV2Factory.sol';
@@ -8,6 +10,7 @@ import './interfaces/IERC20.sol';
 import './interfaces/IWETH.sol';
 import './interfaces/V3/ISwapRouter.sol';
 import './interfaces/V3/IUniswapV3Factory.sol';
+import './interfaces/V3/IUniswapV3Pool.sol';
 import 'hardhat/console.sol';
 
 contract Arbitrage {
@@ -17,6 +20,7 @@ contract Arbitrage {
   ISwapRouter public swapRouterV3;
   address public swapRouterV2 = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
   address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+  address private constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 
   enum Direction { V2ToV3, V3ToV2 }
 
@@ -28,7 +32,7 @@ contract Arbitrage {
   function arbitrageV2ToV3(
     address input,
     address output,
-    uint amountIn, //amountIn may not be needed
+    uint amountIn, //amountIn may not be needed. usually 0
     uint amountOut
   ) external {
     console.log('startArbitrage');
@@ -46,27 +50,42 @@ contract Arbitrage {
   }
 
 
+  struct FlashCallbackData {
+    address tokenIn;
+    address tokenOut;
+    uint256 amount;
+    address payer;
+  }
 
   function arbitrageV3ToV2(
-    address input,
-    address output,
-    uint amountIn, //amountIn may not be needed
-    uint amountOut
-  ) external {
+    address input,//token to be withdrawn from pool
+    address output,//token to be swapped to
+    uint amount //token amount to be withdrawn from pool
+  ) external payable{
     console.log('startArbitrageV3toV2');
-    address poolAddress = IUniswapV3Factory(factoryV3).getPool(input, output, 3000);
+    // pool address for asset withdrawal(borrow)
+    //TODO flash pool should not be hardcoded
+    address poolAddress = IUniswapV3Factory(factoryV3).getPool(USDC, input, 500);
     require(poolAddress != address(0), 'This pool does not exist');
-    console.log('Pool Address');
+    console.log('Withdrawal Pool Address');
     console.log(poolAddress);
 
+    address token0 = IUniswapV3Pool(poolAddress).token0();
+    (uint amount0, uint amount1) = input == token0 ? (amount, uint(0)) : (uint(0), amount);
 
-
-    //    address token0 = IUniswapV2Pair(pairAddress).token0();
-    //
-    //    (uint amount0, uint amount1) = input == token0 ? (uint(0), amountOut) : (amountOut, uint(0));
-    //    //Specify only output amount and make inputAmount 0. input token will be returned in uniswapV2Call
-    //    IUniswapV2Pair(pairAddress).swap(amount0, amount1, address(this), bytes('not empty'));
-
+    IUniswapV3Pool(poolAddress).flash(
+        address(this),
+        amount0,
+        amount1,
+        abi.encode(
+          FlashCallbackData({
+            tokenIn: input,
+            tokenOut: output,
+            amount: amount,
+            payer: msg.sender
+          })
+        )
+      );
 
   }
 
@@ -100,7 +119,7 @@ contract Arbitrage {
     path[0] = _amount0 == 0 ? token1 : token0;
     path[1] = _amount0 == 0 ? token0 : token1;
 
-    //token is token acquired from V2 pair pool
+    //token is the token acquired from V2 pair pool
     IERC20 token = IERC20(_amount0 == 0 ? token1 : token0);
     //otherToken is the token witch will be returned to V2 after V3 swapping
     IERC20 otherToken = IERC20(_amount0 == 0 ? token0 : token1);
@@ -150,4 +169,123 @@ contract Arbitrage {
     console.log(otherToken.balanceOf(address(this)));
     //otherToken.transfer(tx.origin, amountReceived - amountRequired);
   }
+
+
+  function uniswapV3FlashCallback(
+    uint256 fee0,
+    uint256 fee1,
+    bytes calldata data
+  ) external {
+    FlashCallbackData memory decoded = abi.decode(data, (FlashCallbackData));
+    decoded = abi.decode(data, (FlashCallbackData));
+
+//    CallbackValidation.verifyCallback(factory, decoded.poolKey);
+
+    address poolAddress = IUniswapV3Factory(factoryV3).getPool(decoded.tokenIn, decoded.tokenOut, 3000);
+    require(poolAddress != address(0), 'This pool does not exist');
+    console.log('V3 Swap Pool Address');
+    console.log(poolAddress);
+    console.log('WETH');
+    console.log(IERC20(decoded.tokenIn).balanceOf(address(this)));
+    console.log('BAT');
+    console.log(IERC20(decoded.tokenOut).balanceOf(address(this)));
+
+    require(
+      msg.sender == IUniswapV3Factory(factoryV3).getPool(IUniswapV3Pool(msg.sender).token0(), IUniswapV3Pool(msg.sender).token1(), IUniswapV3Pool(msg.sender).fee()),
+      'Unauthorized'
+    );
+    require(fee0 == 0 || fee1 == 0, 'withdraw only one token');
+    require(IERC20(decoded.tokenIn).balanceOf(address(this)) >= decoded.amount, 'withdrawn token do not have sufficient balance');
+
+    //swap withdraw token to another one at V2 protocol
+    uint256 TokenOutV2Amount = swapTokensAtV2(decoded.tokenIn, decoded.tokenOut, decoded.amount);
+    console.log(TokenOutV2Amount);
+
+    console.log('WETH');
+    console.log(IERC20(decoded.tokenIn).balanceOf(address(this)));
+    console.log('BAT');
+    console.log(IERC20(decoded.tokenOut).balanceOf(address(this)));
+
+    //swap whole swapped token to original one at V3 protocol
+    TransferHelper.safeApprove(decoded.tokenOut, address(swapRouterV3), TokenOutV2Amount);
+    uint BAT_balance = IERC20(decoded.tokenOut).balanceOf(address(this));
+
+    // profitable check
+    // exactInputSingle will fail if this amount not met
+
+    //this is the returned amount of tokenIn to original withdrawn pool
+    uint256 amountOwned = fee0 == 0 ? LowGasSafeMath.add(decoded.amount, fee1) : LowGasSafeMath.add(decoded.amount, fee0);
+    console.log('swapped amount at V2');
+    console.log(amountOwned);
+
+    // call exactInputSingle for swapping token1 for token0 in pool w/fee2
+    uint256 amountOutV3 =
+    swapRouterV3.exactInputSingle(
+      ISwapRouter.ExactInputSingleParams({
+          tokenIn: decoded.tokenOut,
+          tokenOut: decoded.tokenIn,
+          fee: 3000,
+          recipient: address(this),
+          deadline: block.timestamp + 200,
+          amountIn: TokenOutV2Amount,
+          amountOutMinimum: 0,
+          sqrtPriceLimitX96: 0
+        })
+    );
+
+    console.log('Complete swap at V3');
+    console.log('WETH');
+    console.log(IERC20(decoded.tokenIn).balanceOf(address(this)));
+    console.log('BAT');
+    console.log(IERC20(decoded.tokenOut).balanceOf(address(this)));
+
+    //return borrowed currency to V3 Pool
+    TransferHelper.safeApprove(decoded.tokenIn, address(this), amountOwned);
+    if (amountOwned > 0) {
+      TransferHelper.safeTransfer(decoded.tokenIn, msg.sender, amountOwned);
+    }
+    // if profitable pay profits to payer
+    if (amountOutV3 > amountOwned) {
+      uint256 profit = LowGasSafeMath.sub(amountOutV3, amountOwned);
+      TransferHelper.safeApprove(decoded.tokenIn, address(this), profit);
+      TransferHelper.safeTransfer(decoded.tokenIn, decoded.payer, profit);
+    }
+
+  }
+
+  function swapTokensAtV2(address tokenIn, address tokenOut, uint256 amountIn)
+  internal
+  returns (uint256 amountOut){
+    address[] memory path;
+
+    if (tokenIn == WETH || tokenOut == WETH) {
+      path = new address[](2);
+      path[0] = tokenIn;
+      path[1] = tokenOut;
+    } else {
+      path = new address[](3);
+      path[0] = tokenIn;
+      path[1] = WETH;
+      path[2] = tokenOut;
+    }
+
+    //[amountIn, amountOut]
+    uint[] memory amounts = UniswapV2Library.getAmountsOut(factoryV2, amountIn, path);
+    uint amountOutMin = amounts[1];
+
+    require(IERC20(tokenIn).balanceOf(address(this)) >= amountIn, 'withdrawn token is less than claimed amount');
+
+    //then we will call swapExactTokensForTokens
+    //for the deadline we will pass in block.timestamp
+    //the deadline is the latest time the trade is valid for
+    TransferHelper.safeApprove(tokenIn, address(swapRouterV2), amountIn);
+    uint[] memory v2swappedAmount = IUniswapV2Router02(swapRouterV2).swapExactTokensForTokens(amountIn, amountOutMin, path, address(this), block.timestamp+10000);
+    console.log(v2swappedAmount.length);
+    for (uint i=0; i<v2swappedAmount.length; i++) {
+      console.log(v2swappedAmount[i]);
+    }
+    return v2swappedAmount[v2swappedAmount.length-1];
+  }
+
+
 }
